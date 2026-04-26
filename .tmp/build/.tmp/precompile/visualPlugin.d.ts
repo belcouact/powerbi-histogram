de8b3a6d6749d1b8549a28c33db9788e15e62604
alex <5525899@qq.com>
2026-04-26 09:55:51 +0800

    Remove build artifacts from tracking

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var histogramVisualE7A47FC60C2E490595136C0B8DCCF4B3: IVisualPlugin;
-export default histogramVisualE7A47FC60C2E490595136C0B8DCCF4B3;
